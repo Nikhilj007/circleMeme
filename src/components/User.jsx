47a9c436d9e13431c711dab4b6dd44394b65123f
Nikhil Jha <nikhiljhanov15@gmail.com>
2024-01-11 22:53:32 +0530

    Update User component styling

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,17 +3,17 @@ import { MdArrowBack } from "react-icons/md";
 
 const User = () => {
   return (
-    <div className="w-full">
-      <div className="flex text-lg p-3 justify-start gap-14 font-semibold items-center">
+    <div className="w-full bg-red-100">
+      <div className="flex text-lg bg-white p-3 justify-start gap-14 font-semibold items-center">
         <div className="text-2xl">
           <MdArrowBack />
         </div>
         <div>Amarya Paul</div>
       </div>
-      <div className="h-96 w-full">
+      <div className="h-96 w-full bg-white">
         <img className="h-96 w-full" src={profile} alt="" />
       </div>
-      <div className="w-11/12 text-start mx-auto">
+      <div className="w-[97%] rounded-md text-start p-4 bg-white mx-auto">
         <div className="font-bold">About</div>
         <div className="text-gray-500">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam saepe, accusantium blanditiis sed numquam quas. Error expedita et dolore obcaecati autem deleniti reiciendis velit eius!

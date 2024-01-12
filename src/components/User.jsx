e482c91f1297385fba576ab3915d96943e38f19a
Nikhil Jha <nikhiljhanov15@gmail.com>
2024-01-12 18:37:57 +0530

    Add edit icon to User component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -7,6 +7,7 @@ import { IoLocationOutline } from "react-icons/io5";
 import { GoPeople } from "react-icons/go";
 import { motion } from "framer-motion";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { GrFormEdit } from "react-icons/gr";
 
 const User = () => {
   const [show, setShow] = useState(true);
@@ -28,66 +29,76 @@ const User = () => {
           <div className="text-2xl">
             <MdArrowBack />
           </div>
-          <div className="-translate-x-20" >Amarya Paul</div>
-          <div onClick={()=>setShow(!show)} className="text-xl">
+          <div className="-translate-x-20">Amarya Paul</div>
+          <div onClick={() => setShow(!show)} className="text-xl">
             <BsThreeDotsVertical />
           </div>
         </div>
         <div className="h-96 w-full bg-white">
-        <img className="h-96 w-full" src={profile} alt="" />
-      </div>
-      <div className="w-[97%] rounded-md mt-2 text-start p-4 bg-white mx-auto">
-        <div className="font-bold">About</div>
-        <div className="text-gray-500">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
-          saepe, accusantium blanditiis sed numquam quas. Error expedita et
-          dolore obcaecati autem deleniti reiciendis velit eius!
+          <img className="h-96 w-full" src={profile} alt="" />
         </div>
-      </div>
-      <div className="w-[97%] rounded-md mt-2 text-start flex flex-col gap-6 p-4 bg-white mx-auto">
-        <div className="flex justify-start gap-3 items-center">
-          <div className="text-3xl">
-            <FiLock />
+        <div className="w-[97%] rounded-md mt-2 text-start p-4 bg-white mx-auto">
+          <div className="font-bold flex justify-between items-center">
+            About
+            <div className="text-2xl border-[1px] border-black rounded-full p-1"><GrFormEdit /></div>
           </div>
-          <div>
-            <div className="font-bold ">College</div>
-            <div className="text-gray-500 text-sm">
-              St. {"Joseph's"} College, Darjeeling{" "}
+          <div className="text-gray-500">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
+            saepe, accusantium blanditiis sed numquam quas. Error expedita et
+            dolore obcaecati autem deleniti reiciendis velit eius!
+          </div>
+        </div>
+        <div className="w-[97%] rounded-md mt-2 text-start flex flex-col gap-6 p-4 bg-white mx-auto">
+          <div className="flex justify-start gap-3 items-center">
+            <div className="text-3xl">
+              <FiLock />
             </div>
-          </div>
-        </div>
-        <div className="flex justify-start gap-3 items-center">
-          <div className="text-3xl">
-            <IoLocationOutline />
-          </div>
-          <div>
-            <div className="font-bold ">Kolkata, India</div>
-          </div>
-        </div>
-        <div className="flex justify-start gap-3 items-center">
-          <div className="text-3xl">
-            <IoEyeOutline />
-          </div>
-          <div>
-            <div className="font-bold  w-full">Department</div>
-            <div className="text-gray-500 text-sm">
-              {"Master's"} of Communication and Journalism
+            <div>
+              <div className="font-bold ">College</div>
+              <div className="text-gray-500 text-sm">
+                St. {"Joseph's"} College, Darjeeling{" "}
+              </div>
             </div>
+            <div className="text-2xl border-[1px] border-black rounded-full p-1"><GrFormEdit /></div>
           </div>
+          <div className="flex justify-start gap-3 items-center">
+            <div className="text-3xl">
+              <IoLocationOutline />
+            </div>
+            <div>
+              <div className="font-bold ">Kolkata, India</div>
+            </div>
+            <div className="text-2xl border-[1px] border-black rounded-full p-1"><GrFormEdit /></div>
+
+          </div>
+          <div className="flex justify-start gap-3 items-center">
+            <div className="text-3xl">
+              <IoEyeOutline />
+            </div>
+            <div>
+              <div className="font-bold  w-full">Department</div>
+              <div className="text-gray-500 text-sm">
+                {"Master's"} of Communication and Journalism
+              </div>
+            </div>
+            <div className="text-2xl border-[1px] border-black rounded-full p-1"><GrFormEdit /></div>
+
+          </div>
+          <div className="flex justify-start gap-3 items-center">
+            <div className="text-3xl">
+              <GoPeople />
+            </div>
+            <div>
+              <div className="font-bold ">amryap@gmail.com</div>
+            </div>
+          <div className="text-2xl border-[1px] border-black rounded-full p-1"><GrFormEdit /></div>
+          </div>
+
         </div>
-        <div className="flex justify-start gap-3 items-center">
-          <div className="text-3xl">
-            <GoPeople />
-          </div>
-          <div>
-            <div className="font-bold ">amryap@gmail.com</div>
-          </div>
-        </div>
-      </div>
         <div className="fixed "></div>
-          <motion.div
-           className="w-full text-center text-blue-600 text-lg fixed bottom-0 rounded-[2.5rem] bg-white mb-3 pb-10 pt-3"
-           initial={{ opacity: 0, y: "100%" }}
+        <motion.div
+          className="w-full text-center text-blue-600 text-lg fixed bottom-0 rounded-[2.5rem] bg-white mb-3 pb-10 pt-3"
+          initial={{ opacity: 0, y: "100%" }}
           animate={show ? { opacity: 1, y: 0 } : {}}
           exit={{ opacity: 0, y: "100%" }}
           transition={{
@@ -96,12 +107,12 @@ const User = () => {
             stiffness: 200,
             damping: 12,
           }}
-           >
-            <div>Change Picture</div>
-            <div>Follow</div>
-            <div>Drop Message</div>
-            <div>Send Crush</div>
-          </motion.div>
+        >
+          <div>Change Picture</div>
+          <div>Follow</div>
+          <div>Drop Message</div>
+          <div>Send Crush</div>
+        </motion.div>
       </div>
     </div>
   );

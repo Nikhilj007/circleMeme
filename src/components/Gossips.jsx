37e5192aacefc0724139a8b740900758d5cf1839
Nikhil Jha <nikhiljhanov15@gmail.com>
2024-01-13 16:48:32 +0530

    Add search functionality to Gossips component

--- src/components/Gossips.jsx
+++ src/components/Gossips.jsx
@@ -2,42 +2,35 @@ import { MdArrowBack } from "react-icons/md";
 import { BsQuestionCircle } from "react-icons/bs";
 import {  GoTriangleUp } from "react-icons/go";
 import { BiSearchAlt } from "react-icons/bi";
-
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 import Question from "./Questions";
+
 function Gossips() {
   const navigate= useNavigate();
   return (
     <div className="bg-zinc-300 w-full mb-14">
-      <div className="flex items-center bg-white py-3 justify-around w-full">
-        <div className="flex gap-3">
-          <button onClick={()=>navigate(-1)} className="text-3xl">
-            <MdArrowBack />
-          </button>
-          <div className="bg-gray-600 px-3 py-1 rounded-sm text-white flex items-center">
-            <div className="text-xl">
-              <BiSearchAlt />
-            </div>
-            Search
-          </div>
-        </div>
-        <div className="flex gap-3">
-          <div className="bg-black text-white rounded-sm flex items-center px-3 py-1">
-            <div className="bg-white text-black rounded-full">
-              <BsQuestionCircle />
-            </div>{" "}
-            Ask A Question
-          </div>
-          <div className="bg-black text-white p-2 rounded-full">
-            <GoTriangleUp />
-          </div>
+      <div to={'/search'} className="fixed bg-white left-6 w-[97%] max-w-lg mx-auto flex items-center border-[1px] border-gray-300 -ml-[1.15rem] px-1">
+        <button onClick={() => navigate(-1)} className="text-3xl">
+          <MdArrowBack />
+        </button>
+        <input
+          onChange={()=>navigate('/search')}
+          className="w-11/12 p-3 focus:outline-none"
+          type="text"
+          placeholder="search something"
+        />
+        <div>
+          <FaSearch />
         </div>
       </div>
+      <div className="mt-10">
       <Question/>
       <Question/>
       <Question/>
       <Question/>
       <Question/>
+      </div>
       
     </div>
   );

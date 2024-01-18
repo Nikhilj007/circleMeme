bbcb019e9d75e40ba27c65c0b633656debde34a8
Nikhil Jha <nikhiljhanov15@gmail.com>
2024-01-18 23:48:14 +0530

    Update textarea cols in CreatePost component

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -58,7 +58,7 @@ function CreatePost() {
           onChange={(e) => setText(e.target.value)}
           className="outline-none resize-none mt-1"
           placeholder="What's on your mind?"
-          cols={width > 570 ? "60" : `40`}
+          cols={width > 570 ? "60" : `30`}
           rows="5"
         ></textarea>
         <input

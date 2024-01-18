86e09b1c86fec396d65b0789e177ce0545712f1f
Nikhil Jha <nikhiljhanov15@gmail.com>
2024-01-19 00:53:32 +0530

    Add question input field and useWindowDimensions hook to Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,9 +2,12 @@ import PropTypes from "prop-types";
 import { RxCross2 } from "react-icons/rx";
 import { FaDotCircle } from "react-icons/fa";
 import { useEffect, useState } from "react";
+import useWindowDimensions from "../hooks/useWindowDimension";
 
 const Modal = ({ isOpen, onClose }) => {
     const [show,setShow]=useState(false)
+    const [question,setQuestion]=useState('')
+    const {width}=useWindowDimensions()
     useEffect(()=>{ 
         if(isOpen)setShow(false)
     }
@@ -12,27 +15,32 @@ const Modal = ({ isOpen, onClose }) => {
   return (
     <>
       <div
-        onClick={onClose}
-        className={`fixed inset-0 z-50 w-full flex mt-16 justify-center ${
+        className={`fixed inset-0 z-10 w-full flex mt-16 justify-center ${
           isOpen ? "visible" : "invisible"
         }`}
       >
         <div
-          className={`absolute -top-10 right-[10%] z-50 ${
+          className={`absolute -top-10 right-[10%] ${
             isOpen ? "visible" : "invisible"
           }`}
           onClick={onClose}
         >
           <RxCross2 className="text-3xl " />
         </div>
-        <div className="fixed inset-0  backdrop-filter backdrop-blur-sm w-full "></div>
+        <div
+          onClick={onClose}
+         className="fixed inset-0 backdrop-blur-sm w-full  "></div>
         <div className="absolute bg-white p-3 rounded-md">
           <div className="w-full bg-gray-600 flex items-center  rounded-sm text-white p-4">
-            <input
-              className="w-full bg-transparent placeholder:text-white outline-none text-lg focus:outline-none"
-              type="text"
-              placeholder="Write your question here"
-            />
+            <textarea
+            className="w-full bg-transparent placeholder:text-white outline-none text-lg focus:outline-none"
+          id="post"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+          placeholder="Write your question here"
+          cols={width > 570 ? "60" : `30`}
+          rows="5"
+        ></textarea>
           </div>
           <div onClick={()=>setShow(!show)} className="flex bg-black items-center gap-2 w-fit mt-4 px-4 py-2 rounded-sm">
             <div className="text-white text-sm">Ask a question</div>

04419f85f96fd7e587c53c0b1a0c6a102e272b34
Nikhil Jha <nikhiljhanov15@gmail.com>
2024-01-22 13:43:19 +0530

    Refactor Modal component to improve readability and add new features

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,56 +1,58 @@
 import PropTypes from "prop-types";
-import { FaDotCircle } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import useWindowDimensions from "../hooks/useWindowDimension";
 
 const Modal = ({ isOpen, onClose }) => {
-    const [show,setShow]=useState(false)
-    const [question,setQuestion]=useState('')
-    const {width}=useWindowDimensions()
-    useEffect(()=>{ 
-        if(isOpen)setShow(false)
-    }
-    ,[isOpen])
+  const [show, setShow] = useState(false);
+  const [question, setQuestion] = useState("");
+  const { width } = useWindowDimensions();
+  useEffect(() => {
+    if (isOpen) setShow(false);
+  }, [isOpen]);
   return (
     <>
       <div
-        className={`fixed inset-0 z-10 w-full flex mt-16 justify-center ${
+        className={`fixed inset-0 z-10 w-full flex mt-40 justify-center ${
           isOpen ? "visible" : "invisible"
         }`}
       >
         <div
           onClick={onClose}
-         className="fixed inset-0 backdrop-blur-sm w-full  "></div>
+          className="fixed inset-0 bg-black opacity-60  w-full  "
+        ></div>
         <div className="absolute bg-white p-3 rounded-md">
           <div className="w-full bg-gray-600 flex items-center  rounded-sm text-white p-4">
             <textarea
-            className="w-full bg-transparent placeholder:text-white outline-none text-lg focus:outline-none"
-          id="post"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-          placeholder="Write your question here"
-          cols={width > 570 ? "60" : `30`}
-          rows="5"
-        ></textarea>
+              className="w-full bg-transparent placeholder:text-white outline-none text-lg focus:outline-none"
+              id="post"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              placeholder="Write your question here"
+              cols={width > 570 ? "60" : `30`}
+              rows="5"
+            ></textarea>
           </div>
-          <div onClick={()=>setShow(!show)} className="flex bg-black items-center gap-2 w-fit mt-4 px-4 py-2 rounded-sm">
-            <div className="text-white text-sm">Ask a question</div>
+          <div className="flex gap-2">
+            <div
+              onClick={() => setShow(!show)}
+              className="flex bg-black items-center gap-2 w-fit mt-4 px-4 py-2 rounded-sm"
+            >
+              <div className="text-white text-sm">Ask a question</div>
+            </div>
+            {show && (
+              <div className="relative w-fit h-fit  bg-white px-2 rounded-md text-start z-50">
+                <div className="flex gap-4 bg-black text-white">
+                  <div className="text-2xl"></div>
+                  Post anonymously
+                </div>
+                <div className="flex gap-4 bg-black text-white">
+                  <div className="text-2xl"></div>
+                  Post as yourself
+                </div>
+              </div>
+            )}
           </div>
         </div>
-        {show && <div className="relative w-9/12 h-fit p-7 bg-white top-32 text-start z-50">
-          <div className="flex gap-4">
-            <div className="text-2xl">
-              <FaDotCircle />
-            </div>
-            Post anonymously
-          </div>
-          <div className="flex gap-4">
-            <div className="text-2xl">
-              <FaDotCircle />
-            </div>
-            Post as yourself
-          </div>
-        </div>}
       </div>
     </>
   );

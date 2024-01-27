882d5cbe1ad8050b90473346828ba9e1b764c52c
Nikhil Jha <nikhiljhanov15@gmail.com>
2024-01-27 22:40:48 +0530

    jhbj

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react/', // Adjust this path to match your subdirectory
+  // base: '/react/', // Adjust this path to match your subdirectory
   plugins: [react()],
 })
 
